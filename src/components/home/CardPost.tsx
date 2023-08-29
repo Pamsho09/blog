7dc66d54526fa5301bea0b09595afcb0589c4994
Jorge Assaf <jorgeassaf160@gmail.com>
2023-08-29 10:06:41 -0600

    usar css variables para evitar problemas con los colores

--- src/components/home/CardPost.tsx
+++ src/components/home/CardPost.tsx
@@ -12,7 +12,7 @@ function CardPost({
 }) {
   return (
     <div className="w-full m-0 bg-gradient-to-tr p-[1px]  from-violet-500  to-orange-300 rounded-[0px] ">
-      <div className="bg-black p-4 m-0 flex flex-col justify-betwee space-y-2">
+      <div className="bg-background p-4 m-0 flex flex-col justify-betwee space-y-2">
         <h4 className="text-lg font-bold ">{title}</h4>
         <p className="text-sm">{description}</p>
         <div className="w-full font-extralight text-sm">
